ee8fd22c3c68a607b3af4420bbca4f58b8f30d79
Lorenzo Cherubin <53137532+lolelu@users.noreply.github.com>
2022-11-16 16:51:35 +0100

    Backend Routes

--- config/twill-navigation.php
+++ config/twill-navigation.php
@@ -1,4 +1,99 @@
 <?php
 
 return [
+    'members' => [
+        'title' => 'Soci',
+        'module' => true,
+    ],
+
+    'static' => [
+        'title' => 'Static',
+        'route' => 'admin.static.associazione.chi_siamo.index',
+        'primary_navigation' => [
+            'associazione' => [
+                'title' => 'Associazione',
+                'secondary_navigation' => [
+                    'chi_siamo' => [
+                        'title' => 'Chi siamo',
+                        'module' => true,
+                    ],
+                    'storia' => [
+                        'title' => 'Storia',
+                        'module' => true,
+                    ],
+                    'organigramma' => [
+                        'title' => 'Organigramma',
+                        'module' => true,
+                    ],
+                ],
+            ],
+            'gruppi_merceologici' => [
+                'title' => 'Gruppi merceologici',
+                'module' => true,
+            ],
+            'servizi' => [
+                'title' => 'Servizi',
+                'module' => true,
+            ],
+            'contatti' => [
+                'title' => 'Contatti',
+                'module' => true,
+            ],
+            'registrazione' => [
+                'title' => 'Registrazione',
+                'module' => true,
+            ],
+            'privacy' => [
+                'title' => 'Privacy',
+                'secondary_navigation' => [
+                    'privacy' => [
+                        'title' => 'Privacy',
+                        'module' => true,
+                    ],
+                    'cookie' => [
+                        'title' => 'Cookie',
+                        'module' => true,
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'content' => [
+        'title' => 'Contenuti',
+        'route' => 'admin.content.pubblicazioni.index',
+        'primary_navigation' => [
+            'pubblicazioni' => [
+                'title' => 'Pubblicazioni',
+                'module' => true,
+            ],
+            'indici' => [
+                'title' => 'Indici',
+                'module' => true,
+            ],
+            'news_eventi' => [
+                'title' => 'News e eventi',
+                'module' => true,
+            ],
+            'comunicati_stampa' => [
+                'title' => 'Comunicati stampa',
+                'module' => true,
+            ],
+        ],
+    ],
+
+    'settings' => [
+        'title' => 'Impostazioni',
+        'route' => 'admin.settings.seo_data.index',
+        'primary_navigation' => [
+            'seo_data' => [
+                'title' => 'SEO',
+                'module' => true,
+            ],
+            'metadata' => [
+                'title' => 'Metadata',
+                'module' => true,
+            ],
+        ],
+    ],
 ];
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,38 @@
 <?php
 
+use Google\Service\AnalyticsData\Row;
 use Illuminate\Support\Facades\Route;
 
 // Register Twill routes here eg.
 // Route::module('posts');
+
+// Register Twill routes here eg.
+Route::module('members');
+
+Route::group(['prefix' => 'settings'], function () {
+    Route::module('seo_data');
+    Route::module('metadata');
+});
+
+Route::group(['prefix' => 'static'], function () {
+    Route::group(['prefix' => 'associazione'], function () {
+        Route::module('chi_siamo');
+        Route::module('storia');
+        Route::module('organigramma');
+    });
+    Route::module('gruppi_merceologici');
+    Route::module('servizi');
+    Route::module('contatti');
+    Route::module('registrazione');
+    Route::group(['prefix' => 'privacy'], function () {
+        Route::module('privacy');
+        Route::module('cookie');
+    });
+});
+
+Route::group(['prefix' => 'content'], function () {
+    Route::module('pubblicazioni');
+    Route::module('indici');
+    Route::module('news_eventi');
+    Route::module('comunicati_stampa');
+});
